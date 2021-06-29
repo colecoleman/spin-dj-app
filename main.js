6f964d8460864f43a60e8118b3775029a99a8fe8
Cole Coleman <colecoleman@me.com>
2021-06-29 10:41:59 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import BaseCard from "./components/UI/BaseCard.vue";
+import TheHeader from "./components/Header/TheHeader.vue";
+import store from './store/index.js';
+import dayjs from "dayjs";
+import {jsPDF} from 'jspdf';
+import html2canvas from 'html2canvas'
+import vueApexCharts from 'vue3-apexcharts';
+
+
+const weekday = require("dayjs/plugin/weekday");
+const weekOfYear = require("dayjs/plugin/weekOfYear");
+
+dayjs.extend(weekday);
+dayjs.extend(weekOfYear);
+
+
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(dayjs)
+  .use(vueApexCharts)
+  .use(jsPDF)
+  .use(html2canvas)
+  .mount("#app");
+
+
+App.component("base-card", BaseCard);
+App.component("the-header", TheHeader);
