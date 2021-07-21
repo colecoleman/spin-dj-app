b1a954e663cb4e69e38f2695b414f3d337747291
colecoleman <74744044+colecoleman@users.noreply.github.com>
2021-07-20 19:10:58 -0500

    Added contactPageIndex.js as an index folder

---
+++ src/views/AdminViews/AdminViewSpecificContactPage/ContactPageComponents/contactPageIndex.js
@@ -0,0 +1,13 @@
+import ContactPageAutomation from './ContactPageAutomation.vue';
+import ContactCardCompany from './ContactCardCompany.vue';
+import ContactCardPerson from './ContactCardPerson.vue';
+import ContactPageToDoList from './ContactPageToDoList.vue';
+import ContactPageUpcomingEvents from './ContactPageUpcomingEvents.vue';
+
+export {
+    ContactPageAutomation,
+    ContactCardCompany,
+    ContactCardPerson,
+    ContactPageToDoList,
+    ContactPageUpcomingEvents
+}
