5808a52e7b1cb510e2a4cdb4acf470adb2fccb6e
colecoleman <74744044+colecoleman@users.noreply.github.com>
2021-08-27 17:49:20 -0500

    Made SVG index for more succint importing

---
+++ src/assets/SVGs/svgIndex.js
@@ -0,0 +1,84 @@
+import AddPackageSVG from './add-package.svg'
+import AddPersonSVG from './add-person.svg'
+import AddTimeClockSVG from './add-time-clock.svg'
+import AutomationSVG from './automation.svg'
+import CalendarSVG from './calendar.svg'
+import CircleArrowUpSVG from './circle-arrow-up.svg'
+import CircleCheckmarkSVG from './circle-checkmark.svg'
+import ClipboardSVG from './clipboard.svg'
+import DiscSVG from './disc.svg'
+import DownArrowSVG from './down-arrow.svg'
+import DropdownArrowSVG from './dropdown-arrow.svg'
+import EditPenSVG from './edit-pen.svg'
+import EmailSVG from './email.svg'
+import EyeIconSVG from './eye-icon.svg'
+import FileOpenSVG from './file-open.svg'
+import FillableCircleSVG from './fillable-circle.svg'
+import FireColorSVG from './fire-color.svg'
+import FireSVG from './fire.svg'
+import GroupPeopleSVG from './group-people.svg'
+import HealthSignSVG from './health-sign.svg'
+import InfoIconSVG from './info-icon.svg'
+import KeySVG from './key.svg'
+import LeftArrowSVG from './left-arrow.svg'
+import LocationAddSVG from './location-add.svg'
+import LocationCircleIconSVG from './location-circle-icon.svg'
+import LocationMarkerSVG from './location-marker.svg'
+import MessageBubbleSVG from './message-bubble.svg'
+import MetricChartSVG from './metricchart.svg'
+import NeutralSVG from './neutral.svg'
+import NotificationBellSVG from './notification-bell.svg'
+import PassingTimeSVG from './passing-time.svg'
+import PersonSVG from './person.svg'
+import PlusSignSVG from './plus-sign.svg'
+import ReferCircleSVG from './refer-circle.svg'
+import RightArrowSVG from './right-arrow.svg'
+import SnowflakeColorSVG from './snowflake-color.svg'
+import SnowflakeSVG from './snowflake.svg'
+import SortAlphaSVG from './sort-alpha.svg'
+import TrashCanSVG from './trash-can.svg'
+import XIconSVG from './x-icon.svg'
+
+
+ export {
+    AddPackageSVG, 
+    AddPersonSVG, 
+    AddTimeClockSVG,  
+    AutomationSVG, 
+    CalendarSVG, 
+    CircleArrowUpSVG,
+    CircleCheckmarkSVG, 
+    ClipboardSVG, 
+    DiscSVG, 
+    DownArrowSVG, 
+    DropdownArrowSVG, 
+    EditPenSVG, 
+    EmailSVG, 
+    EyeIconSVG, 
+    FileOpenSVG, 
+    FillableCircleSVG, 
+    FireColorSVG, 
+    FireSVG, 
+    GroupPeopleSVG, 
+    HealthSignSVG, 
+    InfoIconSVG,  
+    KeySVG, 
+    LeftArrowSVG,
+    LocationAddSVG, 
+    LocationCircleIconSVG,
+    LocationMarkerSVG, 
+    MessageBubbleSVG, 
+    MetricChartSVG, 
+    NeutralSVG, 
+    NotificationBellSVG, 
+    PassingTimeSVG, 
+    PersonSVG, 
+    PlusSignSVG, 
+    ReferCircleSVG, 
+    RightArrowSVG, 
+    SnowflakeColorSVG, 
+    SnowflakeSVG, 
+    SortAlphaSVG, 
+    TrashCanSVG, 
+    XIconSVG 
+}
