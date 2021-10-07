5bd72e72b5dbfab366bf1e6a2c876a1ceb23e6c9
colecoleman <74744044+colecoleman@users.noreply.github.com>
2021-10-07 14:16:31 -0500

    Changed font to Lato and removed old css

--- src/App.vue
+++ src/App.vue
@@ -60,7 +60,7 @@ export default {
 </script>
 
 <style>
-@import url(https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap);
+@import url("https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,400;1,700;1,900&display=swap");
 
 html,
 body {
@@ -74,7 +74,7 @@ body {
 }
 
 #app {
-  font-family: Montserrat, Helvetica, Arial, sans-serif;
+  font-family: Lato, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -129,21 +129,4 @@ textarea {
 .router-link-exact-active {
   color: var(--highlightColor);
 }
-
-/* .fade1-enter-active {
-  animation: fade1 0.5s;
-}
-
-.fade1-leave-active {
-  animation: fade1 0.5s reverse;
-}
-
-@keyframes fade1 {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-} */
 </style>
