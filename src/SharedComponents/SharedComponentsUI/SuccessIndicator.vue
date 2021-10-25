944a14e7d6961cfef6469ead29c47d24e177a10d
colecoleman <74744044+colecoleman@users.noreply.github.com>
2021-10-25 16:30:12 -0500

    Added Success Indicator

---
+++ src/SharedComponents/SharedComponentsUI/SuccessIndicator.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="successes-wrapper" v-if="successes.length > 0">
+    <img :src="XIconSVG" alt="" @click="clearSuccesses()" />
+    <h5 v-for="(successes, index) in successs" :key="index">
+      {{ successes }}
+    </h5>
+  </div>
+</template>
+
+<script>
+import XIconSVG from "../../assets/SVGs/x-icon.svg";
+
+export default {
+  data() {
+    return {
+      XIconSVG,
+    };
+  },
+  methods: {
+    clearSuccesses() {
+      this.$store.dispatch("clearSuccesses");
+    },
+  },
+  props: ["successes"],
+  created() {
+    setTimeout(this.clearSuccesses, 5000);
+  },
+};
+</script>
+
+<style scoped>
+.successes-wrapper {
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  height: fit-content;
+  padding: 10px;
+  padding-right: 25px;
+  width: fit-content;
+  background-color: rgb(64, 230, 72);
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: black;
+}
+
+h5 {
+  text-transform: capitalize;
+}
+
+img {
+  height: 10px;
+  width: 10px;
+  position: absolute;
+  right: 10px;
+  top: 10px;
+}
+</style>
