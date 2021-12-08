f31e1a38dd8c6911baeae4b8a18fdfaa3210d7f1
colecoleman <74744044+colecoleman@users.noreply.github.com>
2021-12-07 19:26:30 -0600

    Removed console logs

--- src/main.js
+++ src/main.js
@@ -4,31 +4,14 @@ import router from "./router";
 import store from './store/index.js';
 import dayjs from "dayjs";
 import jsPDF from 'jspdf';
-// import html2canvas from 'html2canvas'
-
 import maplibregl from "maplibre-gl";
 import BaseCard from './SharedComponents/SharedComponentsUI/BaseCard.vue';
 import Amplify from "aws-amplify";
-// import Location from 'aws-sdk/clients/location';
 import awsconfig from './aws-exports';
 
 Amplify.configure(awsconfig);
 
 import 'maplibre-gl/dist/maplibre-gl.css'
-
-// const createClient = async () => {
-//     const credentials = await Auth.currentCredentials();
-//     const client = new Location({
-//         credentials,
-//         region: awsconfig.aws_project_region,
-//    });
-//    return client;
-// }
-// console.log(createClient)
-
-// const identityPoolId = 'us-east-2:a62d04ff-5884-44f9-847d-fdc319aba576';
-// const mapName = 'SampleMap';
-// const credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: identityPoolId})
 
 const weekday = require("dayjs/plugin/weekday");
 const weekOfYear = require("dayjs/plugin/weekOfYear");
