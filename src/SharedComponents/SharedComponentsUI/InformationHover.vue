47609f40622a3322c7c1b6f6e8646e89ea1785d5
colecoleman <74744044+colecoleman@users.noreply.github.com>
2022-01-06 20:22:46 -0600

    Added a hover-info popup

---
+++ src/SharedComponents/SharedComponentsUI/InformationHover.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="information-hover-wrapper">
+    <base-card>
+      <template v-slot:title>{{ heading }}</template>
+      <template v-slot:content
+        ><p>{{ body }}</p></template
+      >
+    </base-card>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {},
+  components: {},
+  props: ["body", "heading"],
+};
+</script>
+<style scoped>
+.information-hover-wrapper {
+  width: 300px;
+  height: fit-content;
+  position: absolute;
+  top: 0;
+}
+</style>
