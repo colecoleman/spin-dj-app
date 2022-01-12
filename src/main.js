da296d78608af1e52f75186acddd9937822ee3a8
colecoleman <colecoleman@me.com>
2022-01-12 15:00:52 -0600

    Potentially breaking changes with syncing laptop with git

--- src/main.js
+++ src/main.js
@@ -1,41 +1,35 @@
-import { createApp} from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from './store/index.js';
+import store from "./store/index.js";
 import dayjs from "dayjs";
-import jsPDF from 'jspdf';
+import jsPDF from "jspdf";
 import maplibregl from "maplibre-gl";
-import BaseCard from './SharedComponents/SharedComponentsUI/BaseCard.vue';
-import ButtonStandardWithIcon from './SharedComponents/SharedComponentsUI/ButtonStandardWithIcon.vue'
-import { Amplify} from "aws-amplify";
-import AmplifyVue from '@aws-amplify/ui-vue';
-import awsconfig from './aws-exports';
-import 'maplibre-gl/dist/maplibre-gl.css'
+import BaseCard from "./SharedComponents/SharedComponentsUI/BaseCard.vue";
+import ButtonStandardWithIcon from "./SharedComponents/SharedComponentsUI/ButtonStandardWithIcon.vue";
+import { Amplify } from "aws-amplify";
+import AmplifyVue from "@aws-amplify/ui-vue";
+import awsconfig from "./aws-exports";
+import "maplibre-gl/dist/maplibre-gl.css";
 import {
   applyPolyfills,
   defineCustomElements,
-} from '@aws-amplify/ui-components/loader';
-import '@aws-amplify/ui-vue/styles.css';
+} from "@aws-amplify/ui-components/loader";
+import "@aws-amplify/ui-vue/styles.css";
 
 Amplify.configure(awsconfig);
 applyPolyfills().then(() => {
   defineCustomElements(window);
 });
 
-
-
 const weekday = require("dayjs/plugin/weekday");
 const weekOfYear = require("dayjs/plugin/weekOfYear");
 dayjs.extend(weekday);
 dayjs.extend(weekOfYear);
 
-
-
-
-
 createApp(App)
-  .component('base-card', BaseCard)
-  .component('button-standard-with-icon', ButtonStandardWithIcon)
+  .component("base-card", BaseCard)
+  .component("button-standard-with-icon", ButtonStandardWithIcon)
   .use(router)
   .use(store)
   .use(dayjs)
@@ -44,5 +38,3 @@ createApp(App)
   .use(Amplify)
   .use(AmplifyVue)
   .mount("#app");
-
-
