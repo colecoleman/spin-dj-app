f29a07c2cb60257fba8a9007175ce5507ad2d4c6
colecoleman <colecoleman@me.com>
2022-01-14 14:56:21 -0600

    added SVGs

--- src/assets/SVGs/svgIndex.js
+++ src/assets/SVGs/svgIndex.js
@@ -1,91 +1,95 @@
-import AddPackageSVG from './add-package.svg'
-import AddPersonSVG from './add-person.svg'
-import AddTimeClockSVG from './add-time-clock.svg'
-import AutomationSVG from './automation.svg'
-import CalendarSVG from './calendar.svg'
-import CircleArrowUpSVG from './circle-arrow-up.svg'
-import CircleCheckmarkSVG from './circle-checkmark.svg'
-import ClipboardSVG from './clipboard.svg'
-import DiscSVG from './disc.svg'
-import DownArrowSVG from './down-arrow.svg'
-import DropdownArrowSVG from './dropdown-arrow.svg'
-import EditPenSVG from './edit-pen.svg'
-import EmailSVG from './email.svg'
-import ExclamationSVG from './exclamation-mark.svg'
-import EyeIconSVG from './eye-icon.svg'
-import FileOpenSVG from './file-open.svg'
-import FillableCircleSVG from './fillable-circle.svg'
-import FireColorSVG from './fire-color.svg'
-import FireSVG from './fire.svg'
-import GreenCheckmark from './green-checkmark.svg'
-import GroupPeopleSVG from './group-people.svg'
-import HealthSignSVG from './health-sign.svg'
-import InfoIconSVG from './info-icon.svg'
-import KeySVG from './key.svg'
-import LeftArrowSVG from './left-arrow.svg'
-import LocationAddSVG from './location-add.svg'
-import LocationCircleIconSVG from './location-circle-icon.svg'
-import LocationMarkerSVG from './location-marker.svg'
-import MessageBubbleSVG from './message-bubble.svg'
-import MetricChartSVG from './metricchart.svg'
-import NeutralSVG from './neutral.svg'
-import NotificationBellSVG from './notification-bell.svg'
-import PassingTimeSVG from './passing-time.svg'
-import PersonSVG from './person.svg'
-import PlusSignSVG from './plus-sign.svg'
-import RedXCircle from './red-x-circle.svg'
-import ReferCircleSVG from './refer-circle.svg'
-import RightArrowSVG from './right-arrow.svg'
-import SnowflakeColorSVG from './snowflake-color.svg'
-import SnowflakeSVG from './snowflake.svg'
-import SortAlphaSVG from './sort-alpha.svg'
-import TrashCanSVG from './trash-can.svg'
-import XIconSVG from './x-icon.svg'
+import AddPackageSVG from "./add-package.svg";
+import AddPersonSVG from "./add-person.svg";
+import AddTimeClockSVG from "./add-time-clock.svg";
+import AutomationSVG from "./automation.svg";
+import CalendarSVG from "./calendar.svg";
+import CircleArrowUpSVG from "./circle-arrow-up.svg";
+import CircleCheckmarkSVG from "./circle-checkmark.svg";
+import ClipboardSVG from "./clipboard.svg";
+import DiscSVG from "./disc.svg";
+import DownArrowSVG from "./down-arrow.svg";
+import DropdownArrowSVG from "./dropdown-arrow.svg";
+import EditPenSVG from "./edit-pen.svg";
+import EmailSVG from "./email.svg";
+import ExclamationSVG from "./exclamation-mark.svg";
+import EyeIconSVG from "./eye-icon.svg";
+import FileOpenSVG from "./file-open.svg";
+import FillableCircleSVG from "./fillable-circle.svg";
+import FireColorSVG from "./fire-color.svg";
+import FireSVG from "./fire.svg";
+import GreenCheckmark from "./green-checkmark.svg";
+import GroupPeopleSVG from "./group-people.svg";
+import HealthSignSVG from "./health-sign.svg";
+import InfoIconSVG from "./info-icon.svg";
+import KeySVG from "./key.svg";
+import LeftArrowSVG from "./left-arrow.svg";
+import LocationAddSVG from "./location-add.svg";
+import LocationCircleIconSVG from "./location-circle-icon.svg";
+import LocationMarkerSVG from "./location-marker.svg";
+import MessageBubbleSVG from "./message-bubble.svg";
+import MetricChartSVG from "./metricchart.svg";
+import NeutralSVG from "./neutral.svg";
+import NotificationBellSVG from "./notification-bell.svg";
+import PassingTimeSVG from "./passing-time.svg";
+import PersonSVG from "./person.svg";
+import PlusSignSVG from "./plus-sign.svg";
+import RedXCircle from "./red-x-circle.svg";
+import ReferCircleSVG from "./refer-circle.svg";
+import RightArrowSVG from "./right-arrow.svg";
+import SnowflakeColorSVG from "./snowflake-color.svg";
+import SnowflakeSVG from "./snowflake.svg";
+import SortAlphaSVG from "./sort-alpha.svg";
+import TrashCanSVG from "./trash-can.svg";
+import ToolboxSVG from "./toolbox.svg";
+import WrenchGearSVG from "./wrench-gear.svg";
+import XIconSVG from "./x-icon.svg";
 
 let SVGs = {
-        AddPackageSVG, 
-        AddPersonSVG, 
-        AddTimeClockSVG,  
-        AutomationSVG, 
-        CalendarSVG, 
-        CircleArrowUpSVG,
-        CircleCheckmarkSVG, 
-        ClipboardSVG, 
-        DiscSVG, 
-        DownArrowSVG, 
-        DropdownArrowSVG, 
-        EditPenSVG, 
-        EmailSVG, 
-        ExclamationSVG,
-        EyeIconSVG, 
-        FileOpenSVG, 
-        FillableCircleSVG, 
-        FireColorSVG, 
-        FireSVG, 
-        GreenCheckmark,
-        GroupPeopleSVG, 
-        HealthSignSVG, 
-        InfoIconSVG,  
-        KeySVG, 
-        LeftArrowSVG,
-        LocationAddSVG, 
-        LocationCircleIconSVG,
-        LocationMarkerSVG, 
-        MessageBubbleSVG, 
-        MetricChartSVG, 
-        NeutralSVG, 
-        NotificationBellSVG, 
-        PassingTimeSVG, 
-        PersonSVG, 
-        PlusSignSVG, 
-        RedXCircle,
-        ReferCircleSVG, 
-        RightArrowSVG, 
-        SnowflakeColorSVG, 
-        SnowflakeSVG, 
-        SortAlphaSVG, 
-        TrashCanSVG, 
-        XIconSVG, 
-    };
+  AddPackageSVG,
+  AddPersonSVG,
+  AddTimeClockSVG,
+  AutomationSVG,
+  CalendarSVG,
+  CircleArrowUpSVG,
+  CircleCheckmarkSVG,
+  ClipboardSVG,
+  DiscSVG,
+  DownArrowSVG,
+  DropdownArrowSVG,
+  EditPenSVG,
+  EmailSVG,
+  ExclamationSVG,
+  EyeIconSVG,
+  FileOpenSVG,
+  FillableCircleSVG,
+  FireColorSVG,
+  FireSVG,
+  GreenCheckmark,
+  GroupPeopleSVG,
+  HealthSignSVG,
+  InfoIconSVG,
+  KeySVG,
+  LeftArrowSVG,
+  LocationAddSVG,
+  LocationCircleIconSVG,
+  LocationMarkerSVG,
+  MessageBubbleSVG,
+  MetricChartSVG,
+  NeutralSVG,
+  NotificationBellSVG,
+  PassingTimeSVG,
+  PersonSVG,
+  PlusSignSVG,
+  RedXCircle,
+  ReferCircleSVG,
+  RightArrowSVG,
+  SnowflakeColorSVG,
+  SnowflakeSVG,
+  SortAlphaSVG,
+  ToolboxSVG,
+  TrashCanSVG,
+  WrenchGearSVG,
+  XIconSVG,
+};
 
- export default SVGs;
+export default SVGs;
