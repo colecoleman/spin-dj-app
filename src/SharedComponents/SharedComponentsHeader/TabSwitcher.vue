b5f7952d5007642d77f0d456caa2b4141e95c3b1
colecoleman <74744044+colecoleman@users.noreply.github.com>
2022-01-20 19:20:32 -0600

    converted inline svgs to global

--- src/SharedComponents/SharedComponentsHeader/TabSwitcher.vue
+++ src/SharedComponents/SharedComponentsHeader/TabSwitcher.vue
@@ -1,75 +1,26 @@
 <template>
   <div id="tabs">
     <router-link to="/admin/dashboard">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="33"
-        height="28.875"
-        viewBox="0 0 33 28.875"
-      >
-        <path
-          id="Icon_awesome-toolbox"
-          data-name="Icon awesome-toolbox"
-          d="M32.4,13.084,29.48,10.167a2.063,2.063,0,0,0-1.459-.6H24.75V4.406a3.094,3.094,0,0,0-3.094-3.094H11.344A3.094,3.094,0,0,0,8.25,4.406V9.563H4.979a2.064,2.064,0,0,0-1.459.6L.6,13.084A2.063,2.063,0,0,0,0,14.542v5.333H8.25V18.844a1.031,1.031,0,0,1,1.031-1.031h2.063a1.031,1.031,0,0,1,1.031,1.031v1.031h8.25V18.844a1.031,1.031,0,0,1,1.031-1.031h2.063a1.031,1.031,0,0,1,1.031,1.031v1.031H33V14.542A2.062,2.062,0,0,0,32.4,13.084ZM20.625,9.563h-8.25V5.438h8.25ZM24.75,22.969A1.031,1.031,0,0,1,23.719,24H21.656a1.031,1.031,0,0,1-1.031-1.031V21.938h-8.25v1.031A1.031,1.031,0,0,1,11.344,24H9.281A1.031,1.031,0,0,1,8.25,22.969V21.938H0v6.188a2.063,2.063,0,0,0,2.063,2.063H30.938A2.063,2.063,0,0,0,33,28.125V21.938H24.75Z"
-          transform="translate(0 -1.313)"
-        />
-      </svg>
+      <img :src="SVGs.ToolboxSVG" alt="" />
     </router-link>
     <router-link to="/admin/contacts">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="33"
-        height="21"
-        viewBox="0 0 33 21"
-      >
-        <path
-          id="Icon_material-group"
-          data-name="Icon material-group"
-          d="M24,16.5A4.5,4.5,0,1,0,19.5,12,4.481,4.481,0,0,0,24,16.5Zm-12,0A4.5,4.5,0,1,0,7.5,12,4.481,4.481,0,0,0,12,16.5Zm0,3c-3.495,0-10.5,1.755-10.5,5.25V28.5h21V24.75C22.5,21.255,15.495,19.5,12,19.5Zm12,0c-.435,0-.93.03-1.455.075A6.33,6.33,0,0,1,25.5,24.75V28.5h9V24.75C34.5,21.255,27.495,19.5,24,19.5Z"
-          transform="translate(-1.5 -7.5)"
-        />
-      </svg>
+      <img :src="SVGs.GroupPeopleSVG" alt="" />
     </router-link>
     <router-link to="/admin/config">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="37.075"
-        height="37.935"
-        viewBox="0 0 37.075 37.935"
-      >
-        <g
-          id="Group_518"
-          data-name="Group 518"
-          transform="translate(-270 -20.065)"
-        >
-          <g
-            id="Icon_feather-settings"
-            data-name="Icon feather-settings"
-            transform="translate(273.075 21.065)"
-          >
-            <path
-              id="Path_145"
-              data-name="Path 145"
-              d="M28.475,21.875a2.475,2.475,0,0,0,.495,2.73l.09.09a3,3,0,1,1-4.245,4.245l-.09-.09a2.5,2.5,0,0,0-4.23,1.77v.255a3,3,0,1,1-6,0V30.74a2.475,2.475,0,0,0-1.62-2.265,2.475,2.475,0,0,0-2.73.5l-.09.09A3,3,0,1,1,5.81,24.815l.09-.09a2.5,2.5,0,0,0-1.77-4.23H3.875a3,3,0,1,1,0-6H4.01a2.475,2.475,0,0,0,2.265-1.62,2.475,2.475,0,0,0-.495-2.73l-.09-.09A3,3,0,1,1,9.935,5.81l.09.09a2.475,2.475,0,0,0,2.73.495h.12a2.475,2.475,0,0,0,1.5-2.265V3.875a3,3,0,0,1,6,0V4.01A2.5,2.5,0,0,0,24.6,5.78l.09-.09A3,3,0,1,1,28.94,9.935l-.09.09a2.475,2.475,0,0,0-.495,2.73v.12a2.475,2.475,0,0,0,2.265,1.5h.255a3,3,0,1,1,0,6H30.74a2.475,2.475,0,0,0-2.265,1.5Z"
-              transform="translate(-0.875 -0.875)"
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-            />
-          </g>
-          <path
-            id="Icon_awesome-wrench"
-            data-name="Icon awesome-wrench"
-            d="M27.967,6.01a.66.66,0,0,0-1.108-.3l-4.1,4.1-3.739-.623L18.4,5.44l4.1-4.1a.662.662,0,0,0-.312-1.11,7.894,7.894,0,0,0-9.4,10.2L1.032,22.185A3.525,3.525,0,0,0,6.017,27.17L17.762,15.426A7.906,7.906,0,0,0,27.967,6.01ZM3.525,26a1.322,1.322,0,1,1,1.322-1.322A1.322,1.322,0,0,1,3.525,26Z"
-            transform="translate(270 29.797)"
-          />
-        </g>
-      </svg>
+      <img :src="SVGs.WrenchGearSVG" alt="" />
     </router-link>
   </div>
 </template>
+<script>
+import SVGs from "../../assets/SVGs/svgIndex.js";
+export default {
+  data() {
+    return {
+      SVGs,
+    };
+  },
+};
+</script>
 
 <style scoped>
 #tabs {
@@ -85,15 +36,17 @@
   align-items: center;
   justify-content: space-evenly;
 }
-svg {
+img {
   flex: 1;
-  fill: currentColor;
   width: 25px;
   height: 100%;
 }
 
-a {
-  width: 30%;
-  color: currentColor;
+img:hover {
+  filter: drop-shadow(0px 0px 0.5px var(--textColor));
+}
+
+.active-link {
+  color: var(--highlightColor);
 }
 </style>
