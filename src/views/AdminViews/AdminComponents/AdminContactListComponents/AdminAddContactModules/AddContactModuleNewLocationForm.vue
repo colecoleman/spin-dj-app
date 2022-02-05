11b6451ef71dd9a63eba3b6e266d941cffd33da7
colecoleman <74744044+colecoleman@users.noreply.github.com>
2022-02-05 12:04:27 -0600

    Fixed bug preventing loc from saving on cont list

--- src/views/AdminViews/AdminComponents/AdminContactListComponents/AdminAddContactModules/AddContactModuleNewLocationForm.vue
+++ src/views/AdminViews/AdminComponents/AdminContactListComponents/AdminAddContactModules/AddContactModuleNewLocationForm.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="form-body">
-    <h4>Venue:</h4>
     <h5 :class="errors.name ? 'danger' : ''">Venue Name:</h5>
     <input
       type="text"
@@ -8,66 +7,25 @@
       v-model.trim.lazy="location.name"
     />
     <div class="indention">
-      <h5>Address 1:</h5>
+      <h5>Street Address 1:</h5>
       <input
         type="text"
         placeholder="12345 Address St"
-        v-model.trim.lazy="location.address.streetAddress"
+        v-model.trim.lazy="location.address.streetAddress1"
       />
-      <h5>City:</h5>
+      <h5>Street Address 2: (optional)</h5>
       <input
         type="text"
-        placeholder="City"
-        v-model.trim.lazy="location.address.city"
+        placeholder="Apt 1234"
+        v-model.trim.lazy="location.address.streetAddress2"
       />
-      <h5>State/Province:</h5>
+      <h5>City, State/Province, Zip Code:</h5>
       <input
         type="text"
-        placeholder="State/Province"
-        v-model.trim.lazy="location.address.state"
-      />
-      <h5>Zip Code:</h5>
-      <input
-        type="text"
-        placeholder="12345"
-        v-model.trim.lazy="location.address.zipCode"
+        placeholder="Anywhere, AZ, 12345"
+        v-model.trim.lazy="location.address.cityStateZip"
       />
     </div>
-    <!-- <h4>Contact:</h4>
-    <h5>Pronoun/ Prefix:</h5>
-    <select
-      name="Preferred Pronoun"
-      v-model.trim.lazy="location.contact.pronoun"
-    >
-      <option value="Mr.">Mr.</option>
-      <option value="Mrs.">Mrs.</option>
-      <option value="Ms.">Ms.</option>
-      <option value="Other">Other (please prefix first name)</option>
-    </select>
-    <h5 :class="errors.contact.given_name ? 'danger' : ''">First Name:</h5>
-    <input
-      type="text"
-      placeholder="First Name"
-      v-model.trim.lazy="location.contact.given_name"
-    />
-    <h5 :class="errors.contact.family_name ? 'danger' : ''">Last Name:</h5>
-    <input
-      type="text"
-      placeholder="Last Name"
-      v-model.trim.lazy="location.contact.family_name"
-    />
-    <h5 :class="errors.contact.phoneNumber ? 'danger' : ''">Phone Number:</h5>
-    <input
-      type="tel"
-      placeholder="(123)456-7890"
-      v-model.trim.lazy="location.contact.phoneNumber"
-    />
-    <h5 :class="errors.contact.emailAddress ? 'danger' : ''">Email Address:</h5>
-    <input
-      type="email"
-      placeholder="Email Address"
-      v-model.trim.lazy="location.contact.emailAddress"
-    /> -->
   </div>
   <button-standard-with-icon
     text="Submit"
@@ -85,21 +43,10 @@ export default {
       location: {
         name: undefined,
         address: {
-          streetAddress: undefined,
-          city: undefined,
-          state: undefined,
-          zipCode: undefined,
+          streetAddress1: undefined,
+          streetAddress2: undefined,
+          cityStateZip: undefined,
         },
-        // contact: {
-        //   sendInvitation: false,
-        //   role: "vendor",
-        //   companyName: null,
-        //   pronoun: null,
-        //   given_name: null,
-        //   family_name: null,
-        //   phoneNumber: null,
-        //   username: null,
-        // },
       },
       errors: {
         name: false,
@@ -115,36 +62,30 @@ export default {
   methods: {
     submitContact() {
       this.checkEmptyFields();
-      if (!this.errors.contact.phoneNumber) {
-        this.validatePhoneNumber(this.location.contact.phoneNumber);
-      }
-      if (!this.errors.contact.emailAddress) {
-        this.validateEmailAddress(this.location.contact.emailAddress);
-      }
-      if (
-        Object.values(this.errors.contact).every((item) => item === false) &&
-        this.location.name === false
-      ) {
+      // if (!this.errors.contact.phoneNumber) {
+      //   this.validatePhoneNumber(this.location.contact.phoneNumber);
+      // }
+      // if (!this.errors.contact.emailAddress) {
+      //   this.validateEmailAddress(this.location.contact.emailAddress);
+      // }
+      // if (
+      //   Object.values(this.errors.contact).every((item) => item === false) &&
+      //   this.location.name === false
+      // ) {
+      if (!this.errors.name) {
         this.$store.dispatch("addLocation", this.location);
         this.location = {
           name: undefined,
           address: {
-            streetAddress: undefined,
-            city: undefined,
-            state: undefined,
-            zipCode: undefined,
+            streetAddress1: undefined,
+            streetAddress2: undefined,
+            cityStateZip: undefined,
           },
-          // contact: {
-          //   pronoun: undefined,
-          //   given_name: undefined,
-          //   family_name: undefined,
-          //   phoneNumber: undefined,
-          //   emailAddress: undefined,
-          // },
         };
-      } else {
-        return;
       }
+      // } else {
+      //   return;
+      // }
     },
     checkEmptyFields() {
       let location = this.location;
@@ -160,18 +101,6 @@ export default {
       if (!location.name) {
         this.errors.name = true;
       }
-      // if (!location.contact.given_name) {
-      //   this.errors.contact.given_name = true;
-      // }
-      // if (!location.contact.family_name) {
-      //   this.errors.contact.family_name = true;
-      // }
-      // if (!location.contact.emailAddress) {
-      //   this.errors.contact.emailAddress = true;
-      // }
-      // if (!location.contact.phoneNumber) {
-      //   this.errors.contact.phoneNumber = true;
-      // }
     },
     validatePhoneNumber(num) {
       return num.replace(/[^\d/+]/g, "");
@@ -187,7 +116,7 @@ export default {
 <style scoped>
 .form-body {
   width: 100%;
-  height: 80%;
+  height: 100%;
   overflow-y: scroll;
   display: flex;
   flex-direction: column;
