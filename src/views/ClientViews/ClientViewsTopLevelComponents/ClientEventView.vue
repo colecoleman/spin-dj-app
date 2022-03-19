2e79eb94ff37e92d82846e8d12e357bc672a86f5
colecoleman <74744044+colecoleman@users.noreply.github.com>
2022-03-19 10:40:50 -0500

    BUG FIX: Client Page not printing, CSS error

--- src/views/ClientViews/ClientViewsTopLevelComponents/ClientEventView.vue
+++ src/views/ClientViews/ClientViewsTopLevelComponents/ClientEventView.vue
@@ -1,5 +1,5 @@
 <template>
-  <section v-if="event">
+  <div id="div-wrapper">
     <Transition name="rise">
       <invoice
         :invoice="event.invoice"
@@ -26,38 +26,40 @@
         :locations="event.locations"
       />
     </Transition>
-    <div id="contact-card">
-      <event-page-contact-card :event="event" />
-    </div>
-    <div id="alerts">
-      <event-page-alerts :alerts="eventAlerts" />
-    </div>
-    <div id="button-bar">
-      <four-button-bar-with-drop-down
-        :buttons="buttons"
-        @button-clicked="togglePopup"
-      />
-    </div>
-    <div id="location-scroller">
-      <specific-event-page-location-scroller :event="event" />
-    </div>
-    <div id="make-payment">
-      <event-make-payment-card :event="event" :eventId="event.userId" />
-    </div>
-    <div id="contact-carousel">
-      <event-page-contact-carousel :contacts="event.contacts" />
-    </div>
-    <div id="to-do">
-      <to-do-list
-        listType="event"
-        :event="event"
-        :eventContacts="event.contacts"
-      />
-    </div>
-    <div id="messages">
-      <recent-messages :conversationList="eventConversations" />
-    </div>
-  </section>
+    <section v-if="event">
+      <div id="contact-card">
+        <event-page-contact-card :event="event" />
+      </div>
+      <div id="alerts">
+        <event-page-alerts :alerts="eventAlerts" />
+      </div>
+      <div id="button-bar">
+        <four-button-bar-with-drop-down
+          :buttons="buttons"
+          @button-clicked="togglePopup"
+        />
+      </div>
+      <div id="location-scroller">
+        <specific-event-page-location-scroller :event="event" />
+      </div>
+      <div id="make-payment">
+        <event-make-payment-card :event="event" :eventId="event.userId" />
+      </div>
+      <div id="contact-carousel">
+        <event-page-contact-carousel :contacts="event.contacts" />
+      </div>
+      <div id="to-do">
+        <to-do-list
+          listType="event"
+          :event="event"
+          :eventContacts="event.contacts"
+        />
+      </div>
+      <div id="messages">
+        <recent-messages :conversationList="eventConversations" />
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -219,95 +221,102 @@ export default {
 </script>
 
 <style scoped>
-@media screen {
+#div-wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+  color: var(--textColor);
+}
+section {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: 75px 240px 1fr repeat(4, 275px);
+  gap: 10px;
+}
+
+#contact-card {
+  grid-row: 1 / 2;
+}
+
+#alerts {
+  grid-row: 2/ 3;
+}
+#button-bar {
+  grid-row: 3/ 4;
+}
+
+#location-scroller {
+  grid-row: 4/5;
+}
+
+#make-payment {
+  grid-row: 5/ 6;
+}
+
+#contact-carousel {
+  width: 100%;
+  grid-row: 6/ 7;
+}
+
+#to-do {
+  grid-row: 7 / 8;
+  padding-bottom: 60px;
+}
+
+#messages {
+  display: none;
+  /* grid-row: 3 / 4; */
+}
+@media screen and (min-width: 775px) {
   section {
-    width: 100%;
-    height: 100%;
-    display: grid;
-    grid-template-columns: 100%;
-    grid-template-rows: 75px 240px 1fr repeat(4, 275px);
-    gap: 10px;
+    grid-template-columns: repeat(10, 1fr);
+    grid-template-rows: 75px 240px 1fr;
   }
 
   #contact-card {
+    grid-column: 1 / 4;
     grid-row: 1 / 2;
   }
 
   #alerts {
+    grid-column: 1/ 4;
     grid-row: 2/ 3;
   }
   #button-bar {
-    grid-row: 3/ 4;
+    grid-column: 4/ 11;
+    grid-row: 1/ 2;
   }
 
   #location-scroller {
-    grid-row: 4/5;
+    grid-column: 4/ 7;
+    grid-row: 2/3;
   }
 
   #make-payment {
-    grid-row: 5/ 6;
+    grid-column: 7 / 11;
+    grid-row: 2/ 3;
   }
 
   #contact-carousel {
-    width: 100%;
-    grid-row: 6/ 7;
+    grid-column: 1/ 5;
+    grid-row: 3/ 4;
   }
 
   #to-do {
-    grid-row: 7 / 8;
-    padding-bottom: 60px;
+    grid-column: 5 / 8;
+    grid-row: 3 / 4;
+    padding: 0;
   }
 
   #messages {
-    display: none;
-    /* grid-row: 3 / 4; */
-  }
-  @media (min-width: 775px) {
-    section {
-      grid-template-columns: repeat(10, 1fr);
-      grid-template-rows: 75px 240px 1fr;
-    }
-
-    #contact-card {
-      grid-column: 1 / 4;
-      grid-row: 1 / 2;
-    }
-
-    #alerts {
-      grid-column: 1/ 4;
-      grid-row: 2/ 3;
-    }
-    #button-bar {
-      grid-column: 4/ 11;
-      grid-row: 1/ 2;
-    }
-
-    #location-scroller {
-      grid-column: 4/ 7;
-      grid-row: 2/3;
-    }
-
-    #make-payment {
-      grid-column: 7 / 11;
-      grid-row: 2/ 3;
-    }
-
-    #contact-carousel {
-      grid-column: 1/ 5;
-      grid-row: 3/ 4;
-    }
-
-    #to-do {
-      grid-column: 5 / 8;
-      grid-row: 3 / 4;
-      padding: 0;
-    }
-
-    #messages {
-      grid-column: 8 / 11;
-      grid-row: 3 / 4;
-      display: unset;
-    }
+    grid-column: 8 / 11;
+    grid-row: 3 / 4;
+    display: unset;
   }
 }
 
