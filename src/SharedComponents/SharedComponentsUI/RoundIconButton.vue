5fcc758961fe9e5840b4e9b180cc1e44d10855ec
colecoleman <74744044+colecoleman@users.noreply.github.com>
2022-04-21 18:48:28 -0500

    Added Round Icon Button

---
+++ src/SharedComponents/SharedComponentsUI/RoundIconButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="round-button-wrapper">
+    <vue-svg :svg="svg" />
+  </div>
+</template>
+<script>
+import VueSvg from "../../assets/VueSvg.vue";
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {},
+  components: {
+    VueSvg,
+  },
+  props: ["svg"],
+};
+</script>
+<style scoped>
+.round-button-wrapper {
+  width: 35px;
+  height: 35px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 20px;
+  box-sizing: border-box;
+  padding: 5px;
+  background-color: var(--foregroundColor);
+  border: 1px solid var(--cardOutline);
+  margin: 10px;
+}
+</style>
