c7488079f5de66255659a4a97ed9bc5fb0a89317
colecoleman <74744044+colecoleman@users.noreply.github.com>
2022-04-21 21:25:20 -0500

    Added contact and location components for DRY

---
+++ src/SharedComponents/SharedComponentsUI/ListComponents/ContactProfilePictureAndName.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <profile-picture
+      contact="person"
+      :profilePicture="contact.profilePicture"
+      :customStyle="profilePictureStyling"
+    />
+    <h5 class="contact-name" v-if="contact.userId">
+      {{ contact.given_name }} <br />
+      <span> {{ contact.family_name }}</span>
+    </h5>
+  </div>
+</template>
+<script>
+import ProfilePicture from "../../../assets/ProfilePicture.vue";
+export default {
+  data() {
+    return {
+      profilePictureStyling:
+        "height: 30px; min-height: 30px; min-width: 30px; width: 30px; margin: 5px 5px 5px 0;",
+    };
+  },
+  methods: {},
+  created() {},
+  components: { ProfilePicture },
+  props: ["contact"],
+};
+</script>
+<style scoped>
+div {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.contact-name {
+  font-size: 10pt;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  text-transform: uppercase;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+</style>
---
+++ src/SharedComponents/SharedComponentsUI/ListComponents/LocationNameAndAddressStacked.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="location-identifier">
+    <h4 class="venue-name" v-if="location.name">
+      {{ location.name }}
+    </h4>
+    <div class="location-address" v-if="location.address">
+      <p>{{ location.address.streetAddress1 }}</p>
+      <p>{{ location.address.cityStateZip }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {},
+  components: {},
+  props: ["location"],
+};
+</script>
+<style scoped>
+@media screen and (min-width: 320px) {
+  .location-identifier {
+    font-size: 10pt;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .venue-name,
+  h4 {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-size: 12px;
+  }
+
+  .venue-name {
+    font-size: 9px;
+  }
+}
+@media screen and (min-width: 411px) {
+  .venue-name {
+    font-size: 0.7em;
+  }
+
+  .location-address * {
+    font-size: 0.7em;
+  }
+}
+@media screen and (min-width: 475px) {
+  .venue-name {
+    font-size: 0.8em;
+  }
+
+  .location-address * {
+    font-size: 0.8em;
+    margin: 3px;
+  }
+}
+@media screen and (min-width: 550px) {
+  .venue-name {
+    font-size: 0.9em;
+  }
+
+  .location-address * {
+    font-size: 0.8em;
+    margin: 3px;
+  }
+}
+
+@media screen and (min-width: 650px) {
+  .venue-name {
+    font-size: 1em;
+  }
+
+  .location-address * {
+    font-size: 0.9em;
+    margin: 3px;
+  }
+}
+
+@media screen and (min-width: 800px) {
+  .venue-name {
+    font-size: 0.9em;
+  }
+  .location-address * {
+    font-size: 0.8em;
+    margin: 3px;
+  }
+}
+</style>
