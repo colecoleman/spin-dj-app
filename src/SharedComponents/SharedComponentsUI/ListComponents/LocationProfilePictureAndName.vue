043659c33d08f38e2e1a8fa9950f3bab39936a35
colecoleman <74744044+colecoleman@users.noreply.github.com>
2022-04-22 11:48:47 -0500

    Added Location ProfilePictureAndName

---
+++ src/SharedComponents/SharedComponentsUI/ListComponents/LocationProfilePictureAndName.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="name-and-photo">
+    <profile-picture contact="location" :customStyle="svgStyling" />
+    <h5>
+      {{ location.name }}
+    </h5>
+  </div>
+</template>
+<script>
+import ProfilePicture from "../../../assets/ProfilePicture.vue";
+export default {
+  data() {
+    return {
+      svgStyling:
+        "width: 30px; min-width: 30px; height: 30px; min-height: 30px; margin: 5px 5px 5px 0;",
+    };
+  },
+  components: { ProfilePicture },
+  props: ["location"],
+};
+</script>
+<style scoped>
+.name-and-photo {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.name-and-photo h5 {
+  margin: 1px;
+  font-size: 10pt;
+  text-transform: uppercase;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+</style>
