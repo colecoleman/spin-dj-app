c46ffa0338cedcbc9bb56dc2fdcd28b25c0c2b21
colecoleman <74744044+colecoleman@users.noreply.github.com>
2022-04-22 11:43:32 -0500

    Added Slotted Component for List Item Styling

---
+++ src/SharedComponents/SharedComponentsUI/ListItemStyleWrapper.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="hoverable ? 'hoverable list-item-wrapper' : 'list-item-wrapper'">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props: ["hoverable"],
+};
+</script>
+<style scoped>
+.list-item-wrapper {
+  background-color: var(--foregroundColor);
+  border: 1px solid var(--cardOutline);
+  filter: drop-shadow(0px 1px 1px var(--cardOutline));
+  border-radius: 10px;
+  margin: 10px;
+  padding: 0 20px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.hoverable {
+  cursor: pointer;
+}
+
+.hoverable:hover {
+  filter: drop-shadow(0px 2px 1px var(--cardOutline));
+}
+</style>
