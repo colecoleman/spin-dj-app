bdb2b6f0fa25708eaf5c8135b2a6e7bd163c270e
colecoleman <74744044+colecoleman@users.noreply.github.com>
2022-04-22 11:48:25 -0500

    Added LocationAddress Reusable Component

---
+++ src/SharedComponents/SharedComponentsUI/ListComponents/LocationAddress.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="location-address">
+    <p>{{ location.address.streetAddress1 }}</p>
+    <p>{{ location.address.cityStateZip }}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: ["location"],
+};
+</script>
+<style scoped>
+div {
+  overflow: hidden;
+}
+p {
+  /* margin: 3px; */
+  font-size: 0.6em;
+  font-weight: normal;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  margin: 5px;
+}
+</style>
