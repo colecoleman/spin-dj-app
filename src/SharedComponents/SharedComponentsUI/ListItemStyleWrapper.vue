9b733af28b033431b2a7323221f291e31d1215c3
colecoleman <74744044+colecoleman@users.noreply.github.com>
2022-05-02 11:24:06 -0500

    added optional title to list item style wrapper

--- src/SharedComponents/SharedComponentsUI/ListItemStyleWrapper.vue
+++ src/SharedComponents/SharedComponentsUI/ListItemStyleWrapper.vue
@@ -1,11 +1,14 @@
 <template>
   <div :class="hoverable ? 'hoverable list-item-wrapper' : 'list-item-wrapper'">
+    <h4 v-if="title" class="list-item-title">
+      <span>{{ title }}</span>
+    </h4>
     <slot></slot>
   </div>
 </template>
 <script>
 export default {
-  props: ["hoverable"],
+  props: ["hoverable", "title"],
 };
 </script>
 <style scoped>
@@ -20,8 +23,19 @@ export default {
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
+  position: relative;
 }
-
+.list-item-title {
+  position: absolute;
+  top: -5px;
+  font-size: 10px;
+  left: 5px;
+  filter: drop-shadow(0px 0px 3px var(--foregroundColor));
+}
+.list-item-title span {
+  background-color: var(--foregroundColor);
+  padding: 2px 5px;
+}
 .hoverable {
   cursor: pointer;
 }
