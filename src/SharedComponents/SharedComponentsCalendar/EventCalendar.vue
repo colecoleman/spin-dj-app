6bf1e735a3e635116746bb1da09b5b45a2b731c8
colecoleman <74744044+colecoleman@users.noreply.github.com>
2022-06-07 20:24:32 -0500

    Calendar refactoring and cleanup

--- src/SharedComponents/SharedComponentsCalendar/EventCalendar.vue
+++ src/SharedComponents/SharedComponentsCalendar/EventCalendar.vue
@@ -1,109 +1,62 @@
 <template>
   <base-card
     svg="calendar"
-    actionIcon=""
-    :actionText="`${monthArray[masterMonth]}, ${masterYear}`"
-    v-if="!singleDayViewOpen"
-    title="calendar"
-    @action-one-clicked="floatingMenuOpen = !floatingMenuOpen"
+    actionIcon="day"
+    :title="`${monthArray[month]}, ${year}`"
+    @action-one-clicked="toggleFloatingMenu"
   >
     <template v-slot:dropdownContainer>
-      <div class="right-title-parent">
-        <dual-side-floating-menu-with-list-items
-          :actions="floatingIconActions"
-          :currentLeftSelection="masterYear"
-          :currentRightSelection="masterMonth"
-          v-if="floatingMenuOpen"
-          @actionsClicked="newTimeframeSelected"
+      <dual-side-floating-menu-with-list-items
+        class="right-title-parent"
+        :actions="floatingIconActions"
+        :currentLeftSelection="masterYear"
+        :currentRightSelection="masterMonth"
+        v-if="floatingMenuOpen"
+        @actionsClicked="newTimeframeSelected"
+      />
+    </template>
+    <template v-slot:content v-if="events">
+      <div id="calendar-container">
+        <round-icon-button
+          class="left-side-arrow arrow"
+          @click="monthChange(`backward`)"
+          svg="left-arrow"
+        />
+        <div id="main-calendar-content">
+          <div v-for="(day, index) in WEEKDAYS" class="weekday" :key="index">
+            {{ day }}
+          </div>
+          <div
+            v-for="(day, index) in days"
+            :key="index"
+            :class="{
+              day,
+              isNotCurrentMonth: !day.isCurrentMonth,
+              today: day.isCurrentDay,
+              hasEvents: day.hasEvents,
+            }"
+          >
+            <p>{{ day.dayOfMonth }}</p>
+          </div>
+        </div>
+        <round-icon-button
+          class="right-side-arrow arrow"
+          svg="right-arrow"
+          @click="monthChange(`forward`)"
         />
       </div>
     </template>
-    <template v-slot:content v-if="events">
-      <div id="calendar-container" v-if="!singleDayViewOpen">
-        <div id="body">
-          <div
-            class="left-side-arrow arrow"
-            @click="monthChange(`backward`)"
-            @mousedown.prevent=""
-          >
-            <vue-svg class="svg" svg="left-arrow" />
-          </div>
-          <div id="main-calendar-content">
-            <div id="days">
-              <div v-for="day in WEEKDAYS" class="weekday" :key="day.key">
-                {{ day.identifier }}
-              </div>
-              <div
-                v-for="day in days"
-                :key="day.date"
-                :class="{
-                  day,
-                  isNotCurrentMonth: !day.isCurrentMonth,
-                  today: day.isCurrentDay,
-                  hasEvents: day.hasEvents,
-                }"
-              >
-                <p>{{ day.dayOfMonth }}</p>
-              </div>
-            </div>
-          </div>
-          <div
-            class="right-side-arrow arrow"
-            @click="monthChange(`forward`)"
-            @mousedown.prevent=""
-          >
-            <vue-svg class="svg" svg="right-arrow" />
-          </div>
-        </div>
-      </div>
-    </template>
   </base-card>
-  <single-day-view
-    v-if="singleDayViewOpen"
-    :date="singleDayChosen"
-    :events="events"
-    @close-single-day-view="singleDayViewOpen = false"
-  />
 </template>
 
 <script>
-import SingleDayView from "./SingleDayView/SingleDayView.vue";
+import RoundIconButton from "../SharedComponentsUI/RoundIconButton.vue";
 import DualSideFloatingMenuWithListItems from "../SharedComponentsUI/DualSideFloatingMenuWithListItems.vue";
-import VueSvg from "../../assets/VueSvg.vue";
 
 export default {
   data() {
     return {
-      WEEKDAYS: {
-        sunday: {
-          key: 1,
-          identifier: "S",
-        },
-        monday: {
-          key: 2,
-          identifier: "M",
-        },
-        tuesday: {
-          key: 3,
-          identifier: "T",
-        },
-        wednesday: {
-          key: 4,
-          identifier: "W",
-        },
-        thursday: {
-          key: 5,
-          identifier: "T",
-        },
-        friday: {
-          key: 6,
-          identifier: "F",
-        },
-        saturday: {
-          key: 7,
-          identifier: "S",
-        },
-      },
+      WEEKDAYS: ["S", "M", "T", "W", "T", "F", "S"],
       monthArray: [
         "January",
         "February",
@@ -121,85 +74,54 @@ export default {
       monthChangeCount: 0,
       yearChangeCount: 0,
       floatingMenuOpen: false,
-      singleDayViewOpen: false,
       singleDayChosen: undefined,
       masterMonth: undefined,
       masterYear: undefined,
+      month: new Date().getMonth(),
+      year: new Date().getFullYear(),
     };
   },
-  created: function () {
-    this.masterMonth = this.INITIAL_MONTH + this.yearChangeCount;
-    this.masterYear = this.INITIAL_YEAR;
-  },
+
   methods: {
-    getNumberOfDaysInMonth: function (year, month) {
-      return new Date(year, month + 1, 0).getDate();
+    toggleFloatingMenu() {
+      this.floatingMenuOpen = !this.floatingMenuOpen;
+    },
+
+    getNumberOfDaysInMonth() {
+      let time = this.viewedMonth;
+      let date = new Date(time.getFullYear(), time.getMonth() + 1, 0);
+      return date.getDate();
     },
     getWeekday(date) {
       return date.getDay();
     },
     monthChange(direction) {
-      let count = this.masterMonth;
-      let yearCount = this.yearChangeCount;
-      const countGuards = function () {
-        if (count < 0) {
-          count = 11;
-          yearCount--;
-        }
-        if (count > 11) {
-          count = 0;
-          yearCount++;
-        }
-      };
       if (direction == `forward`) {
-        count++;
-        countGuards();
+        this.month++;
       }
       if (direction == `backward`) {
-        count--;
-        countGuards();
+        this.month--;
       }
-      this.masterMonth = +count;
-      this.masterYear = this.masterYear + yearCount;
-    },
-    newMonthSelected(id) {
-      this.monthChangeCount = 0;
-      this.masterMonth = id + 1;
-      this.toggleMonthSelector();
-    },
-    newYearSelected(year) {
-      this.yearChangeCount = 0;
-      this.masterYear = year;
-    },
-    selectDay(day) {
-      this.singleDayChosen = day.date;
-      this.singleDayViewOpen = true;
     },
     newTimeframeSelected(selections) {
       if (selections.left !== undefined) {
-        this.masterYear = selections.left;
+        this.year = selections.left;
       }
       if (selections.right !== undefined) {
-        this.masterMonth = selections.right;
+        this.month = selections.right;
       }
       this.floatingMenuOpen = false;
     },
   },
   computed: {
-    today() {
-      return new Date();
-    },
-    INITIAL_YEAR: function () {
-      return this.today.getFullYear();
-    },
-    INITIAL_MONTH: function () {
-      return this.today.getMonth();
+    viewedMonth() {
+      return new Date(this.year, this.month);
     },
     surroundingYears() {
       let array = [];
       for (
-        let temporaryYear = this.masterYear - 3;
-        temporaryYear < this.masterYear + 7;
+        let temporaryYear = this.year - 3;
+        temporaryYear < this.year + 7;
         temporaryYear++
       ) {
         array.push(temporaryYear);
@@ -216,70 +138,55 @@ export default {
       return this.events.map((a) => new Date(a.data.date));
     },
     // used to establish the dates shown on calendar
-    currentMonthDays: function () {
-      let year = this.masterYear;
-      let month = this.masterMonth;
+    currentMonthDays() {
+      let daysInMonth = this.getNumberOfDaysInMonth();
       let eventDays = this.daysWithEvents;
-      return [...Array(this.getNumberOfDaysInMonth(year, month))].map(
-        (day, index) => {
-          return {
-            date: new Date(year, month, index + 1),
-            dayOfMonth: index + 1,
-            isCurrentMonth: true,
-            get isCurrentDay() {
-              return this.date
-                .toDateString()
-                .includes(new Date().toDateString());
-            },
-            get hasEvents() {
-              return eventDays.some((x) => {
-                return x.toDateString() == this.date.toDateString();
-              });
-            },
-          };
-        }
-      );
+      return [...Array(daysInMonth)].map((day, index) => {
+        return {
+          date: new Date(this.year, this.month, index + 1),
+          dayOfMonth: index + 1,
+          isCurrentMonth: true,
+          get isCurrentDay() {
+            return this.date.toDateString().includes(new Date().toDateString());
+          },
+          get hasEvents() {
+            return eventDays.some((x) => {
+              return x.toDateString() == this.date.toDateString();
+            });
+          },
+        };
+      });
     },
-    previousMonthDays: function () {
-      let year = this.masterYear;
-      let month = this.masterMonth - 1;
+    previousMonthDays() {
       let firstDayOfTheMonthWeekday = this.getWeekday(
-        this.currentMonthDays[1].date
+        this.currentMonthDays[0].date
       );
       const visibleNumberOfDaysFromPreviousMonth = firstDayOfTheMonthWeekday
-        ? firstDayOfTheMonthWeekday - 1
+        ? firstDayOfTheMonthWeekday
         : 6;
-      const previousMonthLastMondayDayOfMonth = new Date(
-        year,
-        month,
-        `${0 - visibleNumberOfDaysFromPreviousMonth}`
+      const previousMonthLastSundayOfTheMonth = new Date(
+        this.year,
+        this.month,
+        `${1 - visibleNumberOfDaysFromPreviousMonth}`
       ).getDate();
       return [...Array(visibleNumberOfDaysFromPreviousMonth)].map(
-        (day, index) => {
+        (_, index) => {
           return {
-            date: new Date(
-              year,
-              month,
-              previousMonthLastMondayDayOfMonth + index
-            ),
-            dayOfMonth: previousMonthLastMondayDayOfMonth + index,
+            dayOfMonth: previousMonthLastSundayOfTheMonth + index,
             isCurrentMonth: false,
           };
         }
       );
     },
-    nextMonthDays: function () {
-      let year = this.masterYear;
-      let month = this.masterMonth + 1;
+    nextMonthDays() {
       const lastDayOfTheMonthWeekday = this.getWeekday(
         this.currentMonthDays[this.currentMonthDays.length - 1].date
       );
       const visibleNumberOfDaysFromNextMonth = lastDayOfTheMonthWeekday
         ? 6 - lastDayOfTheMonthWeekday
         : 6;
-      return [...Array(visibleNumberOfDaysFromNextMonth)].map((day, index) => {
+      return [...Array(visibleNumberOfDaysFromNextMonth)].map((_, index) => {
         return {
-          date: new Date(year, month, index + 1),
           dayOfMonth: index + 1,
           isCurrentMonth: false,
         };
@@ -296,10 +203,8 @@ export default {
   emits: ["actionsClicked"],
   props: ["events"],
   components: {
-    // TimeSelector,
-    VueSvg,
-    SingleDayView,
     DualSideFloatingMenuWithListItems,
+    RoundIconButton,
   },
 };
 </script>
@@ -307,39 +212,28 @@ export default {
 <style scoped>
 #calendar-container {
   height: 100%;
+  width: 100%;
   color: var(--textColor);
-}
-
-#body {
   display: grid;
   grid-template-columns: repeat(9, 1fr);
-  height: 100%;
-  width: 100%;
+  grid-template-rows: 1;
 }
-
 #main-calendar-content {
+  height: 100%;
   grid-column: 2/9;
-  height: 100%;
-}
-
-#days {
-  width: 100%;
-  height: 100%;
-  display: grid;
-  grid-row: 2/9;
   grid-template-columns: repeat(7, 1fr);
   grid-template-rows: repeat(auto-fill, 1fr);
+  height: 100%;
+  width: 100%;
+  grid-row: 2/9;
   cursor: default;
+  display: grid;
 }
 
-#days div {
+#main-calendar-content div {
   flex: 1;
-
-  /* width: calc(250px / 7); */
   font-size: 12px;
   font-weight: 700;
-  /* height: 35px; */
-  /* width: 35px; */
   display: flex;
   align-items: center;
   justify-content: center;
@@ -373,10 +267,8 @@ export default {
 }
 
 .arrow {
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  margin: auto;
+  grid-row: 1/9;
 }
 
 .today {
