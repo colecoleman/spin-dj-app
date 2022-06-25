6f8b4aabf18d9903899eb9a3f1e5e2ae4791ed9d
colecoleman <74744044+colecoleman@users.noreply.github.com>
2022-06-24 19:12:29 -0500

    redesign: contactInformationEdit

--- src/Components/SharedComponentsContact/ContactInformationEdit.vue
+++ src/Components/SharedComponentsContact/ContactInformationEdit.vue
@@ -1,145 +1,243 @@
 <template>
-  <div class="edit-card-wrapper">
-    <base-card
-      actionIcon="x-icon"
-      @action-one-clicked="closeEditCard()"
-      title="Edit Contact"
-    >
-      <template v-slot:content>
-        <div class="edit-card-inner-wrapper">
-          <select v-model="fieldToEdit">
-            <option v-for="(field, key) in fields" :key="key" :value="key">
-              {{ field.display }}
-            </option>
-          </select>
-          <div v-if="fieldToEdit != undefined">
-            <input-with-title
-              :title="fields[fieldToEdit].display"
-              :type="fieldToEdit.inputType"
-              v-if="fieldToEdit != 'profilePicture'"
-              :inputValue="fields[fieldToEdit].value"
-              @input="fieldInput(fields[fieldToEdit], 'value', $event)"
-              :placeholder="contact[fieldToEdit]"
+  <backdrop @click.self="closeEditCard" id="edit-card-wrapper">
+    <div id="edit-card">
+      <div id="information-edit-section">
+        <div class="information-item">
+          <editable-profile-picture
+            :processing="processing"
+            contact="person"
+            :profilePicture="contact.profilePicture"
+            @update-profile-picture="updateProfilePicture"
+          />
+        </div>
+        <div
+          class="information-item"
+          v-for="(item, key, index) in fields"
+          :key="index"
+        >
+          <p>{{ item.display }}:</p>
+          <div
+            class="information-and-pen"
+            v-if="
+              !checkIfEditArrayContainsField(key) &&
+              item.display !== 'Profile Picture'
+            "
+          >
+            <p>
+              <span>{{ item.data }}</span>
+            </p>
+            <round-icon-button svg="edit-pen" @click="toggleEditField(key)" />
+          </div>
+          <div
+            class="information-and-pen"
+            v-if="
+              checkIfEditArrayContainsField(key) ||
+              item.display === 'Profile Picture'
+            "
+          >
+            <component
+              :is="item.component"
+              v-bind="item.componentParameters"
+              :processing="processing"
+              @input="textInput"
+              @actionable-icon-clicked="actionableIconClicked"
+              @update-profile-picture="updateProfilePicture"
+              @close="toggleEditField(key)"
             />
-            <input
-              v-if="fieldToEdit === 'profilePicture'"
-              type="file"
-              id="contact-card-edit-hidden-file-button"
-              @change="onFileChange"
-              style="display: none"
-            />
-            <div class="button-wrapper">
-              <button-standard-with-icon
-                v-if="fieldToEdit === 'profilePicture'"
-                :text="photoFile ? photoFile.name : 'Choose File'"
-                @click="chooseFile()"
-                class="form-button"
-              />
-            </div>
-            <div class="button-wrapper">
-              <button-standard-with-icon text="Save" @click="saveField()" />
-            </div>
           </div>
         </div>
-      </template>
-    </base-card>
-  </div>
+      </div>
+    </div>
+  </backdrop>
 </template>
 
 <script>
-import InputWithTitle from "../../Components/SharedComponentsUI/ElementLibrary/InputWithTitle.vue";
+import Backdrop from "../SharedComponentsUI/Backdrop.vue";
+import InputWithActionableIcon from "../SharedComponentsUI/ElementLibrary/InputWithActionableIcon.vue";
+import RoundIconButton from "../SharedComponentsUI/RoundIconButton.vue";
+import EditableProfilePicture from "./EditableProfilePicture.vue";
 export default {
   data() {
     return {
       photoFile: undefined,
       fieldToEdit: undefined,
-      fields: {
-        given_name: {
-          display: "First Name",
-          inputType: "text",
-          value: undefined,
-        },
-        family_name: {
-          display: "Last Name",
-          inputType: "text",
-          value: undefined,
-        },
-        pronoun: { display: "Pronoun", inputType: "text", value: undefined },
-        phoneNumber: {
-          display: "Phone Number",
-          inputType: "tel",
-          value: undefined,
-        },
-        email: {
-          display: "Email Address",
-          inputType: "email",
-          value: undefined,
-        },
-        profilePicture: {
-          display: "Profile Picture",
-          inputType: "file",
-          value: undefined,
-        },
-      },
+      activeEditField: undefined,
+      activeEditValue: undefined,
+      processing: false,
     };
   },
-  methods: {
-    fieldInput(object, property, value) {
-      if (object) {
-        object[property] = value;
-      } else {
-        this[property] = value;
-      }
-    },
-    closeEditCard() {
-      this.$emit("closeEditCard");
-    },
-    chooseFile() {
-      document.getElementById("contact-card-edit-hidden-file-button").click();
-    },
-    onFileChange(e) {
-      var files = e.target.files || e.dataTransfer.files;
-      if (!files.length) return;
-      this.photoFile = files[0];
-    },
-    async saveField() {
-      if (this.photoFile) {
-        await this.$store.dispatch("addPhoto", this.photoFile).then((res) => {
-          this.fields.profilePicture.value = res;
-        });
-      }
-      let key = {
-        userId: this.contact.userId,
-        tenantId: this.contact.tenantId,
+  computed: {
+    fields() {
+      const { given_name, family_name, email, phoneNumber, pronoun } =
+        this.contact;
+      return {
+        given_name: {
+          data: given_name,
+          display: "First Name",
+          component: InputWithActionableIcon,
+          componentParameters: {
+            placeholder: given_name,
+            svg: this.processing ? "loading" : "circle-checkmark",
+          },
+        },
+        family_name: {
+          data: family_name,
+          display: "First Name",
+          component: InputWithActionableIcon,
+          componentParameters: {
+            placeholder: family_name,
+            svg: this.processing ? "loading" : "circle-checkmark",
+          },
+        },
+        email: {
+          data: email,
+          display: "Email",
+          component: InputWithActionableIcon,
+          componentParameters: {
+            placeholder: email,
+            svg: this.processing ? "loading" : "circle-checkmark",
+          },
+        },
+        phoneNumber: {
+          data: phoneNumber,
+          display: "Phone Number",
+          component: InputWithActionableIcon,
+          componentParameters: {
+            placeholder: phoneNumber,
+            svg: this.processing ? "loading" : "circle-checkmark",
+          },
+        },
+        pronoun: {
+          data: pronoun,
+          display: "Pronoun",
+          component: InputWithActionableIcon,
+          componentParameters: {
+            placeholder: pronoun,
+            svg: this.processing ? "loading" : "circle-checkmark",
+          },
+        },
       };
+    },
+  },
+  methods: {
+    closeEditCard() {
+      this.$emit("close-edit-card");
+    },
+    textInput(val) {
+      this.activeEditValue = val;
+    },
+    toggleEditField(field) {
+      if (field !== this.activeEditField) {
+        this.activeEditField = field;
+      } else {
+        this.activeEditField = undefined;
+      }
+    },
+    checkIfEditArrayContainsField(field) {
+      if (field === this.activeEditField) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    async updateProfilePicture(picture) {
+      this.activeEditField = "profilePicture";
+      await this.$store.dispatch("addPhoto", picture).then((res) => {
+        this.activeEditValue = res;
+      });
+      this.actionableIconClicked();
+    },
+    async actionableIconClicked() {
+      this.procesing = true;
+      let { userId, tenantId } = this.contact;
+      let key = { userId, tenantId };
       let payload = {
         contactKey: key,
-        variable: this.fieldToEdit,
-        value: this.fields[this.fieldToEdit].value,
+        variable: this.activeEditField,
+        value: this.activeEditValue,
       };
       await this.$store.dispatch("editContact", payload);
+      this.$emit("edit-contact", payload);
+      this.processing = false;
+      this.$emit("close-edit-card");
     },
   },
   components: {
-    InputWithTitle,
+    InputWithActionableIcon,
+    Backdrop,
+    RoundIconButton,
+    EditableProfilePicture,
   },
+  emits: ["close-edit-card", "edit-contact"],
   props: ["contact"],
 };
 </script>
 
 <style scoped>
-.edit-card-wrapper {
+#edit-card-wrapper {
+  z-index: 100;
+  backdrop-filter: blur(3px);
+}
+#edit-card {
+  background-color: var(--foregroundColor);
   position: absolute;
-  right: -50px;
-  top: -100%;
-  z-index: 2;
-  width: 300px;
+  right: 30%;
+  left: calc(50% - 150px);
+  top: calc(50% - 275px);
+  width: 40%;
+  min-width: 300px;
+  height: 550px;
+  border-radius: 10px;
+  filter: drop-shadow(0px 4px 3px var(--cardOutline));
 }
 
-.button-wrapper {
-  padding: 10px;
+#information-edit-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  flex-wrap: wrap;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 10%;
 }
-input {
-  margin: 10px;
+.information-item {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding-bottom: 20px;
+}
+
+.information-and-pen {
+  display: flex;
+  align-items: center;
+  /* position: relative; */
+}
+
+p {
+  text-align: left;
+  margin: 0px;
+}
+
+p > span {
+  font-weight: 600;
+  cursor: pointer;
+}
+@media screen and (min-width: 800px) {
+  #edit-card {
+    left: 30%;
+    top: 32.5%;
+    height: 35%;
+  }
+
+  #information-edit-section {
+    align-items: flex-start;
+  }
+  .information-item {
+    flex: 1 1 50px;
+    max-width: 50%;
+    padding-bottom: unset;
+  }
 }
 </style>
