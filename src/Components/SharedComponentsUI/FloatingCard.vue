aa10f6ac15052762922c600bad7546b50f7061aa
colecoleman <74744044+colecoleman@users.noreply.github.com>
2022-07-02 19:21:35 -0500

    added floating card component

---
+++ src/Components/SharedComponentsUI/FloatingCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="floating-card">
+    <slot></slot>
+  </div>
+</template>
+<style scoped>
+.floating-card {
+  height: fit-content;
+  width: fit-content;
+  position: fixed;
+  padding: 10px;
+  background-color: var(--foregroundColor);
+  border-radius: 10px;
+  filter: drop-shadow(0px 4px 3px var(--cardOutline));
+}
+</style>
