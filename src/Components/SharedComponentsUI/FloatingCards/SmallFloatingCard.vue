05a9c0610879679620289d5bbf82420a11b30ee6
colecoleman <74744044+colecoleman@users.noreply.github.com>
2022-07-12 15:48:17 -0500

    added small and medium floating cards

---
+++ src/Components/SharedComponentsUI/FloatingCards/SmallFloatingCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <backdrop @click.self="close">
+    <floating-card class="large-floating-card">
+      <slot></slot>
+    </floating-card>
+  </backdrop>
+</template>
+<script>
+import FloatingCard from "../FloatingCard.vue";
+import Backdrop from "../../SharedComponentsUI/Backdrop.vue";
+export default {
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+  components: {
+    Backdrop,
+    FloatingCard,
+  },
+  emits: ["close"],
+};
+</script>
+<style scoped>
+.large-floating-card {
+  position: fixed;
+  height: 70%;
+  width: 80%;
+  top: 15%;
+  left: 10%;
+  padding: 0;
+}
+@media screen and (min-width: 800px) {
+  .large-floating-card {
+    left: 30%;
+    top: 30%;
+    padding: 20px;
+
+    height: calc(40% - 40px);
+    width: calc(40% - 40px);
+  }
+}
+</style>
